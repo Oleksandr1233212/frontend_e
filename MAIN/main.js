b774a3fbb43e389be43d1b4aebee765bded4f3e7
Oleksandr1233212 <pp99241979@gmail.com>
2024-06-22 10:29:55 +0300

    second commit

---
+++ MAIN/main.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+createApp(App).mount('#app')
+import 'jquery/dist/jquery.min.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+import 'popper.js/dist/umd/popper.js'
+import 'wow.js'
+import 'easing'
+// import './lib/waypoints/waypoints.min.js'
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
+// import './js/main.js'
+// import router from './router';
